04545b5c0d622f26e89c518bdc37748e28c930d2
5114259 <5114259@163.com>
2013-12-10 10:15:42 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 12-3-24
+ * Time: 下午10:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+         String str = "Hello World";
+         System.out.print(str);
+    }
+
+}
